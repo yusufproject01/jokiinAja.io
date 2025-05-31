e82068f072ac6efb6d0ac1de73e7b3b51e9c9a76
yusufproject01 <yusufproject01@gmail.com>
2025-05-31 22:40:26 +0700

    first commit

---
+++ layout/Navbar.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import { ADLaM_Display, Inter, Poppins } from "next/font/google";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const adlamDisplay = ADLaM_Display({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const poppins = Poppins({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+
+export default function Navbar() {
+    const [scroll, setScroll] = useState(false);
+
+    useEffect(() => {
+      window.addEventListener("scroll", () => {
+        setScroll(window.scrollY > 50);
+      });
+    }, []);
+    
+  return (
+    <nav className={`${scroll ? "bg-(--secondaryRed) text-white border-b-2 border-(--tertiaryRed)" : "bg-transparent"} transition-all duration-300 ease-in-out w-full h-20 flex items-center justify-between px-16 fixed z-50`}>
+      <Link
+        href="/"
+        className={`${adlamDisplay.className} ${scroll ? "hover:text-white text-slate-50" : "hover:text-(--secondaryRed)"} text-4xl font-bold`}
+      >
+        JokiinAja
+      </Link>
+      <ul className={`${inter.className} flex gap-6`}>
+        <li>
+          <Link
+            href="/"
+            className={`${scroll ? "hover:text-white text-slate-50" : "text-black hover:text-(--secondaryRed)"} text-base hover:font-bold `}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className={`${scroll ? "hover:text-white text-slate-50" : "text-black hover:text-(--secondaryRed)"} text-base hover:font-bold `}
+          >
+            Layanan
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className={`${scroll ? "hover:text-white text-slate-50" : "text-black hover:text-(--secondaryRed)"} text-base hover:font-bold `}
+          >
+            Order
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className={`${scroll ? "hover:text-white text-slate-50" : "text-black hover:text-(--secondaryRed)"} text-base hover:font-bold `}
+          >
+            FAQ
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/"
+            className={`${scroll ? "hover:text-white text-slate-50" : "text-black hover:text-(--secondaryRed)"} text-base hover:font-bold `}
+          >
+            Syarat & Ketentuan
+          </Link>
+        </li>
+      </ul>
+      <button className={`${poppins.className} ${scroll ? "bg-slate-100 text-(--primaryRed) hover:bg-(--quartenaryRed)" : "bg-(--secondaryRed) text-white hover:bg-(--primaryRed) "} px-6 py-2 rounded-md hover:cursor-pointer`}>
+            Hubungi Kami
+        </button>
+    </nav>
+  );
+}
---
+++ layout/RootNavbarLayout.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+
+export default function RootNavbarLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+}
